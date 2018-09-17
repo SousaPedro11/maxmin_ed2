a65a2e9efc9728cf9609e67c9066e66f67990c77
Pedro <ppls2106@gmail.com>
2018-09-17 08:49:27 -0300

    add README.md

--- src/maxmin_ed2/MaxMinUtil.java
+++ src/maxmin_ed2/MaxMinUtil.java
@@ -18,7 +18,7 @@ public class MaxMinUtil {
         final int array[] = new int[tam];
 
         final Random rand = new Random();
-        final int max = 99999;
+        final int max = 999999;
         final int min = 0;
         rand.nextInt(((max - min) + 1) + min);
         for (int i = 0; i < tam; i++) {
